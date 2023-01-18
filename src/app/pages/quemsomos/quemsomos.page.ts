8989188a3140a2e7cf55041c868b4e52679f7609
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2023-01-18 08:45:48 -0300

    att2

--- src/app/pages/quemsomos/quemsomos.page.ts
+++ src/app/pages/quemsomos/quemsomos.page.ts
@@ -10,12 +10,13 @@ export class QuemsomosPage implements OnInit {
 
   public quemsomos!: string;
 
-  image = "https://www.showmetech.com.br/wp-content/uploads//2021/11/black-friday-os-melhores-notebooks-e-computadores-para-aproveitar-a-data.png"
 
   constructor(private activatedRoute: ActivatedRoute) { }
+
 
   ngOnInit() {
     this.quemsomos = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
 
+ 
 }
