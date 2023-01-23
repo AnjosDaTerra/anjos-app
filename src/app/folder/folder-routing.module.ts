392a03d9a1fe6163865f8186acf4ea4c09eddda2
yuri1709 <yuriconder184@gmail.com>
2023-01-23 18:08:02 -0300

    deletado a pasta login

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarPage } from '../pages/cadastrar/cadastrar.page';
-import { LoginPage } from '../pages/login/login.page';
 import { QuemsomosPage } from '../pages/quemsomos/quemsomos.page';
 
 import { FolderPage } from './folder.page';
@@ -18,10 +17,6 @@ const routes: Routes = [
   {
     path: '',
     component: CadastrarPage
-  },
-  {
-    path: '',
-    component: LoginPage
   }
 ];
 
