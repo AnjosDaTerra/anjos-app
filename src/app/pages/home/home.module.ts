a922a91e0861c6504a79036aa3b4b24e166a8672
Técnico em Informática 2022.1 <36127512022.1@senacrjedu.local>
2023-02-06 11:35:02 -0300

    Fix footer

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { HomePage } from './home.page';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
+    SharedModule
     ],
   declarations: [HomePage]
 })
