fb0e561e1c294e465d0e34631bfb6dff5aa0c6ab
yuri1709 <yuriconder184@gmail.com>
2023-02-07 18:51:15 -0300

    error msg no login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Usuario } from 'src/app/Core/models/vitima/login.interface';
 import { UtilidadesService } from 'src/app/Core/services/utilidades.service';
 
@@ -10,30 +10,90 @@ import { UtilidadesService } from 'src/app/Core/services/utilidades.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  
-  form1! : FormGroup;
+
   isTextFieldType: boolean | undefined;
   botaoCL: boolean = true;
   botaoCLmsg: string = 'Cadastra-se';
   titulo: string = 'Login';
-
+  //Tenho que ver onde vou por esse httpOptions
   httpOptions = {
     headers: new HttpHeaders({'Content-Type' : 'application/json'})
   } 
+  //---------------------------------
+  form1! : FormGroup
+  error_messages = {
+    'cpf': [
+      { type: 'required',  message: 'Campo obrigatório'},
+      { type: 'minlength', message: 'CPF deve ter 11 algarismos'},
+      { type: 'maxlenght', message: 'CPF deve ter 11 algarismos'}
+    ],
+
+    'email': [
+      { type: 'required',  message: 'Campo obrigatório'},
+      { type: 'minlenght', message: 'Número de caracteres insuficiente'},
+      { type: 'maxlenght', message: 'Você excedeu o número de caracteres'},
+      { type: 'required',  message: 'Por favor, digite um email válido'}
+    ],
+    
+    'password': [
+      { type: 'required',  message: 'Campo obrigatório'},
+      { type: 'minlenght', message: 'Sua senha tem menos de 4 caracteres'},
+      { type: 'maxlenght', message: 'Sua senha passou do tamanho permetido'}
+    ],
+
+    'confirmPassword': [
+      { type: 'required',  message: 'Campo obrigatório'},
+      { type: 'minlenght', message: 'Sua senha tem menos de 4 caracteres'},
+      { type: 'maxlenght', message: 'Sua senha passou do tamanho permetido'}
+    ]
+  }
+
+  constructor(
+    private readonly httpClient: HttpClient, 
+    private util:UtilidadesService, 
+    private formBuilder: FormBuilder
+  ) { }
+
   ngOnInit() {
     this.form1 = this.formBuilder.group({
-      cpf: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, Validators.required],
-      confirmPassword: [],
-    });
+      cpf: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11)
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30),
+        Validators.email
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(30)
+      ])),
+      confirmPassowrd: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(30)
+      ]))
+    }, {
+        validators: this.checkPassword.bind(this)    
+      });
   }
-  constructor(private readonly httpClient: HttpClient, private util:UtilidadesService, private formBuilder: FormBuilder) { }
-   togglePasswordFieldType() {
+
+  togglePasswordFieldType() {
     this.isTextFieldType = !this.isTextFieldType;
   }
 
-  mudarCadastroLogin() {
+  public async checkPassword() {
+    const password = this.form1.value['password'];
+    const confirmPassword = this.form1.value['confirmPassword'];
+    if(password === confirmPassword) { return null } else { return true };
+  }
+
+  
+  public mudarCadastroLogin() {
     this.botaoCL = !this.botaoCL;
     if (this.botaoCL) {
       this.botaoCLmsg ='Cadastra-se'
