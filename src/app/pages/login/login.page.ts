97fda4e0561eb03f85667f7d31b69bdfb46d206d
yuri1709 <yuriconder184@gmail.com>
2023-02-07 11:19:50 -0300

    mexer nas validações dos campos do form1

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/Core/models/vitima/login.interface';
 import { UtilidadesService } from 'src/app/Core/services/utilidades.service';
 
@@ -10,6 +11,7 @@ import { UtilidadesService } from 'src/app/Core/services/utilidades.service';
 })
 export class LoginPage implements OnInit {
   
+  form1! : FormGroup;
   isTextFieldType: boolean | undefined;
   botaoCL: boolean = true;
   botaoCLmsg: string = 'Cadastra-se';
@@ -19,9 +21,14 @@ export class LoginPage implements OnInit {
     headers: new HttpHeaders({'Content-Type' : 'application/json'})
   } 
   ngOnInit() {
-    
+    this.form1 = this.formBuilder.group({
+      cpf: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+      confirmPassword: [],
+    });
   }
-  constructor(private readonly httpClient: HttpClient, private util:UtilidadesService) { }
+  constructor(private readonly httpClient: HttpClient, private util:UtilidadesService, private formBuilder: FormBuilder) { }
    togglePasswordFieldType() {
     this.isTextFieldType = !this.isTextFieldType;
   }
@@ -37,21 +44,24 @@ export class LoginPage implements OnInit {
     }
   }
 
-  public cadastrar(form: any) {
+  public cadastrar() {
     //Pega os valores do formulario
-    let objCadastro: Usuario = {
-      cpf: form.value.cpf,
-      senha: form.value.senha,
-      email: form.value.email
-    }
+     let objCadastro: Usuario = {
+       cpf: this.form1.value['cpf'],
+       senha: this.form1.value['password'],
+       email: this.form1.value['email']
+     }
+    console.log("form",this.form1.value['cpf']);
+    console.log(objCadastro)
+
     //Faz a chamada do endpoint de cadastro
-    this.httpClient.post<Usuario|string>('http://127.0.0.1:4000/vitima/criar-login',objCadastro,this.httpOptions).subscribe((result) => {
-      if(result == "200") {
-        this.util.informando('Cadastro realizado com sucesso!', 'success', 'top', 5000);
-      } else {
-        this.util.informando('CPF já cadastrado!', 'danger', 'top', 5000);
-      }
-    });
+    // this.httpClient.post<Usuario|string>('http://127.0.0.1:4000/vitima/criar-login',objCadastro,this.httpOptions).subscribe((result) => {
+    //   if(result == "200") {
+    //     this.util.informando('Cadastro realizado com sucesso!', 'success', 'top', 5000);
+    //   } else {
+    //     this.util.informando('CPF já cadastrado!', 'danger', 'top', 5000);
+    //   }
+    // });
   }
 
 }
